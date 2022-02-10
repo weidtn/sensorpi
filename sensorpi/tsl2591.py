f318b3aa85072fb76554d87d02ea853c221c8497
Nikolai Weidt <weidtn@gmail.com>
2022-02-10 13:24:33 +0100

    added json format for TSL2591

--- sensorpi/tsl2591.py
+++ sensorpi/tsl2591.py
@@ -3,14 +3,31 @@ import board
 import adafruit_tsl2591
 
 
-def read_tsl2591(sensor="tsl2591", comment=None):
+def read_tsl2591():
     """
-    Reads the sensor over I2C
-    TODO Make return into JSON-like map for influxdb"""
+    Reads the sensor over I2C and returns the sensor class
+    """
     i2c = board.I2C()  # uses board.SCL and board.SDA
     sensor = adafruit_tsl2591.TSL2591(i2c)
     return sensor
 
+def as_json(measurement, sensor_name="TSL2591", comment=None):
+    """
+    Reads the sensor and returns the data in the json format for influxdb
+    """
+    try:
+        sensor = read_tsl2591()
+        json = [{"measurement": measurement,
+                 "tags": {"sensor": sensor_name,
+                          "comment": comment},
+                 "fields": {"Lux": sensor.lux,
+                            "IR": sensor.infrared,
+                            "Vis": sensor.visible,
+                            "Full": sensor.full_spectrum}
+                 }]
+        return json
+    except:
+        print(f"Error reading sensor {sensor_name}. Is it connected?")
 
 def main():
     data = read_tsl2591()
