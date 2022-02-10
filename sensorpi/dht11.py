836f1235a7e467eb2a9c3b484e98f278ac9afe59
Nikolai Weidt <weidtn@gmail.com>
2022-02-10 12:55:37 +0100

    added json implementation for DHT11

--- sensorpi/dht11.py
+++ sensorpi/dht11.py
@@ -7,13 +7,24 @@ def read_dht11(pin):
     # board_pin = eval(f"board.D{pin}")
     board_pin = getattr(board, f"D{pin}")
     sensor = adafruit_dht.DHT11(board_pin, use_pulseio=False)
-    return (sensor.humidity, sensor.temperature)
+    return sensor
+
+def as_json(measurement, pin, sensor_name="DHT11", comment=None):
+    sensor = read_dht11(pin)
+    json = [{"measurement": measurement,
+             "tags": {"sensor": sensor_name,
+                      "comment": comment},
+             "fields": {"temperature": sensor.temperature,
+                       "humidity": sensor.humidity}
+             }]
+    return json
 
 def main(pin=None):
     if pin == None:
         pin = input("Read DHT11 on GPIO Pin#: ")
     try:
-        h,t = read_dht11(pin)
+        sensor = read_dht11(pin)
+        h, t = sensor.humidity, sensor.temperature
         print(f"Humidity: {h}% \nTemperature: {t}°C")
     except:
         print("Error. Is the pin correct?")
