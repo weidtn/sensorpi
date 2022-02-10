978ec6ea421d7ad74cd5f59a5dfc3c778da0acbf
Nikolai Weidt <weidtn@gmail.com>
2022-02-10 15:17:18 +0100

    Added docstrings to dht11

--- sensorpi/dht11.py
+++ sensorpi/dht11.py
@@ -2,14 +2,41 @@
 import board
 import adafruit_dht
 
-def read_dht11(pin):
-    """returns (humidity,temperature) tuple from DHT sensor on given pin"""
-    # board_pin = eval(f"board.D{pin}")
+def read_dht11(pin: int):
+    """Reads the DHT11 sensor connected on the given pin.
+
+    Reads the DHT11 sensor by using the adafruit circuitpython library. It reads the given board pin.
+
+    Args:
+        pin: The pin of the board the sensor is connected to.
+
+    Returns:
+        sensor: the sensor class from adafruit_dht which contains the data as attributes.
+
+    Example:
+        sensor = read_dht11(26)
+        t = sensor.temperature
+        h = sensor.humidity
+        print(t,h)
+
+    """
     board_pin = getattr(board, f"D{pin}")
     sensor = adafruit_dht.DHT11(board_pin, use_pulseio=False)
     return sensor
 
-def as_json(measurement, pin, sensor_name="DHT11", comment=None):
+def as_json(measurement: str, pin: int, sensor_name: str = "DHT11", comment: str = None):
+    """Reads the DHT11 sensor and returns in the correct json format for influxdb.
+
+    Args:
+        measurement: name of the measurement.
+        pin: The pin of the board the sensor is connected to:
+        sensor_name: Name of the sensor, useful if there are more than one. Defaults to DHT11.
+        comment: Optional comment for the sensor/measurement
+
+    Returns:
+        json: the json body for sending the data to the influx database
+
+    """
     sensor = read_dht11(pin)
     json = [{"measurement": measurement,
              "tags": {"sensor": sensor_name,
