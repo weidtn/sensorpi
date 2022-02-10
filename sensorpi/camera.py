4d25088606af266b6d4d87228c057ab99f7f6a41
Nikolai Weidt <weidtn@gmail.com>
2022-02-10 13:49:14 +0100

    added json format to camera

--- sensorpi/camera.py
+++ sensorpi/camera.py
@@ -6,7 +6,9 @@ import cv2
 import numpy as np
 
 def capture(rotate=True):
-    """Caputures image from the camera and returns in as opencv/numpy array"""
+    """
+    Caputures image from the camera and returns in as opencv/numpy array
+    """
     # Create the in-memory stream
     stream = io.BytesIO()
     with picamera.PiCamera() as camera:
@@ -22,13 +24,40 @@ def capture(rotate=True):
     return image
 
 def save_img(path, image):
-    """Saves the image to the path on the filesystem"""
+    """
+    Saves the image to the path on the filesystem
+    """
     cv2.imwrite(path, image)
 
 def calc_histogram(img):
-    """Calculates histogram of grayscale image"""
+    """
+    Calculates histogram of grayscale image
+    """
     hist = cv2.calcHist([img], channels=[0], mask=None, histSize=[256], ranges=[0,256])
     return hist.ravel()
 
+def hist_as_json(measurement, sensor_name="Camera", rotate=True, comment=None):
+    """
+    Returns the integrated histogram value in json format for influxdb
+    """
+    hist = calc_histogram(capture(rotate))
+    int_hist = integrate_histogram(hist)
+    json= [{"measurement": measurement,
+            "tags": {"sensor": sensor_name,
+                     "comment": comment},
+            "fields": {"Integrated Histogram": int_hist}}]
+    return json
+
 def integrate_histogram(hist):
+    """
+    Integrates the histogram
+    TODO: Check method. Different results for numpy/by hand/scipy
+    TODO: Maybe normalize?
+    """
     return np.trapz(hist, dx=1)
+
+def main():
+    print(f"Integrated Histogram: {integrate_histogram(calc_histogram(capture()))}")
+
+if __name__ == "__main__":
+    main()
