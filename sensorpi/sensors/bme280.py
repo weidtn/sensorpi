d7bf73ee6e44e16dc5bc94badd222e308f8b1355
Nikolai Weidt <weidtn@gmail.com>
2022-02-17 08:50:48 +0100

    implementation of SPI for BME280 sensor

--- sensorpi/sensors/bme280.py
+++ sensorpi/sensors/bme280.py
@@ -9,6 +9,23 @@ def read_bme280_i2c(address=0x76):
     return bme280
 
 
+def read_bme280_spi(pin: int):
+    """Reads the BME280 over SPI on selected pin.
+
+    Arguments:
+        pin: The digital pin on the board as integer.
+
+    Returns:
+        bme280: The sensor object containing the data as attributes.
+
+    """
+    # get digital pin from board with integer
+    board_pin = board.__getattribute__(f"D{pin}")
+    cs = digitalio.DigitalInOut(board_pin)
+    spi = board.SPI()
+    bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, cs)
+    return bme280
+
 def i2c_as_json(measurement: str, address=0x76, sensor_name: str = "BME280", comment: str = None):
     try:
         sensor = read_bme280_i2c(address)
@@ -21,4 +38,20 @@ def i2c_as_json(measurement: str, address=0x76, sensor_name: str = "BME280", com
                  }]
         return json
     except:
+        print(f"Error reading sensor {sensor_name}. Is it connected?")
+
+
+def spi_as_json(measurement: str, pin, sensor_name: str = "BME280",
+                comment: str = None):
+    try:
+        sensor = read_bme280_spi(pin)
+        json = [{"measurement": measurement,
+                 "tags": {"sensor": sensor_name,
+                          "comment": comment},
+                 "fields": {"temperature": sensor.temperature,
+                            "pressure": sensor.pressure,
+                            "relative humidity": sensor.relative_humidity}
+                 }]
+        return json
+    except Exception:
         print(f"Error reading sensor {sensor_name}. Is it connected?")
